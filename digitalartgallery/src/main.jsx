e82d315fdd0867f7dcd6c4986f758f9931fbd8e5
Bhargava799 <s.v.bhargava799@gmail.com>
2025-11-30 07:37:53 +0530

    Updated package.json file

--- digitalartgallery/src/main.jsx
+++ digitalartgallery/src/main.jsx
@@ -6,7 +6,7 @@ import DataContent from "./components/DataContent"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Virtual-Art-Gallery-Project-FEDF">
     <DataContent>
       <Routes1 />
     </DataContent>
